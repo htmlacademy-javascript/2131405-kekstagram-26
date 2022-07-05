3448e788dc31045f940155e259bd8ed90849fb41
dlisicin <klub936@gmail.com>
2022-07-05 09:27:51 +0300

    добавлена отрисовка миниатюр

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 import {createPhotosDescriptions} from './data.js';
 import {getRandomValue} from './util.js';
 import {checkStringLength} from './util.js';
+import {createPhoto} from './miniatures.js';
 
 getRandomValue(0,10);
 
 checkStringLength('Я изучаю JS', 20);
+
+createPhoto();
 
 console.log(createPhotosDescriptions());
 
